dc8b21553c3519ff6d4fd4f80fce7e9313464ee7
Ian Douglas Scott <ian@iandouglasscott.com>
2020-04-08 17:11:11 -0700

    Add exterior/render.sh

---
+++ exterior/render.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+res=400x400
+if [ "$1" == "--hidpi" ]; then
+    res=800x800
+fi
+
+mkdir -p out
+for i in *.png
+do
+    echo "$i -> out/$i"
+    convert -scale $res "$i" "out/$i"
+done
