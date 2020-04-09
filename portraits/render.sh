412f2bf9ca09a7d8038237edc9ca2fa363d2c477
Ian Douglas Scott <ian@iandouglasscott.com>
2020-04-08 17:02:37 -0700

    portaits/render.sh: option for hidpi render

--- portraits/render.sh
+++ portraits/render.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+res=200x150
+if [ "$1" == "--hidpi" ]; then
+    res=400x300
+fi
+
 for i in $(find -name '*.xcf')
 do
     mkdir -p out/$(dirname "$i")
     echo "$i -> out/${i%.xcf}.png"
-    ./xcf2png.sh $i out/${i%.xcf}.png
+    ./xcf2png.sh $i out/${i%.xcf}.png $res
 done
