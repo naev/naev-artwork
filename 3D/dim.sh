3c9f6f52c9b846b14101409e2cfe3c545845330e
Edgar Simo-Serra <ess@waseda.jp>
2021-03-16 02:09:21 +0900

    Disable viper

--- 3D/dim.sh
+++ 3D/dim.sh
@@ -98,6 +98,7 @@ function getsize {
    elif [[ "$SHIP" =~ marauder ]]; then SIZE=66;
    elif [[ "$SHIP" =~ ira ]]; then SIZE=112;
    elif [[ "$SHIP" =~ dogma ]]; then SIZE=128;
+   #elif [[ "$SHIP" =~ viper ]]; then SIZE=60; # NOT IN GAME!!
    else
       exit 1
    fi
