36ab87d27a78e383424eaf62f2f6c3477c33eda4
Justin Blanchard <UncombedCoconut@gmail.com>
2021-07-03 12:30:42 -0400

    Tweak SVG logo build process to key off page size.

--- 2D/logo/compare.py
+++ 2D/logo/compare.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 
 subprocess.check_call(['make'])
-roots = ['../../../naev/dat/gfx/logo/', '../../out/logo/']
+roots = ['../../../naev/artwork/gfx/logo/', '../../out/logo/']
 fns = [os.path.join(mid, name) for top in roots for (mid, _, names) in os.walk(top) for name in names]
 fns.sort(key = lambda s: s[::-1])
 subprocess.check_call([
@@ -11,5 +11,5 @@ subprocess.check_call([
     '--draw-filename', '--draw-tinted',
     '--fullscreen',
     '--image-bg', 'checks',
-    '--zoom', '1600',
+    '--zoom', '400',
 ] + fns)
