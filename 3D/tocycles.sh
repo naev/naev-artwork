0a75e42585d352864fa0bb67cabffd0755886def
Edgar Simo-Serra <ess@waseda.jp>
2021-11-01 16:38:18 +0900

    Added script to upgrade models to cycles.

---
+++ 3D/tocycles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+BLENDER="blender-2.7"
+
+if [ -z "$1" ]
+then
+    for i in ships/*.blend
+    do
+        ./tocycles.sh $i
+    done
+else
+    $BLENDER $1 -b -P materials_cycles_converter.py
+fi
