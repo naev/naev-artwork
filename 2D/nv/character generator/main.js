10fc616d9729bff4b45e504b916d6081e250fb29
Dror <dror@imark.co.il>
2022-05-09 12:00:13 +0300

    added export buttons

--- 2D/nv/character generator/main.js
+++ 2D/nv/character generator/main.js
@@ -29,6 +29,10 @@ function main_print(sub_menu = 'presets', cur_id = 'bartender_m1', swatch_id = '
 		</div>
 		<div id="presets"><h2>Choose Preset</h2>${choose_presets}</div>
 		<div id="swatches"><h2>Choose Colors</h2>${choose_swatches}</div>
+	</div>
+	<div id="export_buttons">
+		<button id="export" onclick="save_character_image();">Export as SVG</button>
+		<button id="export" onclick="save_character_image('webp');">Export as WEBP</button>
 	</div>`;
 	
 	
@@ -39,7 +43,9 @@ function main_print(sub_menu = 'presets', cur_id = 'bartender_m1', swatch_id = '
 		
 	portrait_print += get_svg_code(graphic_info, swatch_id);
 	
-	const portrait_html = `<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="0 0 1000 1415">
+	const portrait_html = `<canvas id="hidden_canvas" width="1000" height="1415"></canvas>
+	<div id="linkholder"></div>
+	<svg id="main_portrait" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="0 0 1000 1415">
 		<path class="stamp" fill="#fff" stroke-width="20" stroke="#fff" stroke-linejoin="round" d="${stamp_d}" />
 		${portrait_print}
 	</svg>`;
@@ -86,4 +92,80 @@ function get_svg_code(obj, print_in_white = 'default'){
 function change_parent_class(change_to = '', obj){
 	obj.parentNode.parentNode.classList = change_to;
 	
+}
+
+
+function save_character_image(filetype = 'svg'){
+	
+	const svgElement = document.getElementById('main_portrait');
+	
+	const d = new Date();
+	const t = d.getTime();
+	
+	if(filetype == 'svg') {
+	
+		const svg_pre = `<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+`;
+		let serializer = new XMLSerializer();
+		
+		const source = svg_pre + serializer.serializeToString(svgElement);
+		
+		SAVE_KEY = `character_${t}`;
+		
+		hiddenElement = document.createElement('a');
+		
+		hiddenElement.href = 'data:attachment/svg+xml;charset=utf-8,' + encodeURIComponent(source);
+		hiddenElement.target = '_blank';
+		hiddenElement.download = SAVE_KEY + '.svg';
+		hiddenElement.click();
+		hiddenElement = null;
+	
+	}
+	else if (filetype == 'webp'){
+		
+		let clonedSvgElement = svgElement.cloneNode(true);
+		let outerHTML = clonedSvgElement.outerHTML,
+		blob = new Blob([outerHTML],{type:'image/svg+xml;charset=utf-8'});
+
+		let URL = window.URL || window.webkitURL || window;
+		let blobURL = URL.createObjectURL(blob);
+		  
+		canvas = document.getElementById('hidden_canvas');
+		
+		let imageObj = new Image();
+		imageObj.onload = () => {
+			
+		   canvas.width = 1000;
+		   
+		   canvas.height = 1415;
+		   
+		   let context = canvas.getContext('2d');
+		   // draw image in canvas starting left-0 , top - 0  
+		   context.drawImage(imageObj, 0, 0, 1000, 1415 );
+		
+		};
+		imageObj.src = blobURL;
+		
+		//let png = canvas.toDataURL(); // default png
+		//let jpeg = canvas.toDataURL('image/jpg', 0.85);
+		let webp = canvas.toDataURL('image/webp', 1);
+		
+		let download = function(href, name){
+		  let link = document.createElement('a');
+		  link.download = name;
+		  link.style.opacity = "0";
+		  document.getElementById('linkholder').append(link);
+		  link.href = href;
+		  link.click();
+		  link.remove();
+		}
+		
+		setTimeout(function(){
+			//download(png, `character_${t}.png`);
+			download(webp, `character_${t}.webp`);
+		}, 500);
+		
+		
+	}
 }
