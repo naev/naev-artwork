5791780f3cb1da99879e6ee3f81bf9356875d5f0
Edgar Simo-Serra <ess@waseda.jp>
2024-01-27 13:17:25 +0900

    Fixed some scripts

--- 3D/polygons/blend_to_stl.py
+++ 3D/polygons/blend_to_stl.py
@@ -6,10 +6,18 @@ import bpy
 import os
 
 # Extract name of file
-filename = os.path.basename(bpy.data.filepath)
+filename = os.path.basename( bpy.data.filepath )
 
-# Select layer 1
-bpy.ops.object.select_by_layer(layers=1)
+# Unselect all
+for obj in bpy.data.objects:
+    obj.select_set(False)
+
+# Collection 1 should contain the main ship (old format)
+for obj in bpy.data.collections['Collection 1'].all_objects:
+    if obj.type!="MESH":
+        continue
+
+    obj.select_set(True)
 
 # Export
 bpy.ops.export_mesh.stl("EXEC_DEFAULT",  use_selection=True, filepath=(filename + '.stl'))
--- 3D/polygons/polygonSTL.py
+++ 3D/polygons/polygonSTL.py
@@ -4,7 +4,7 @@
 
 
 # If you added a ship blend:
-# Go to bottom of the file, and replace 
+# Go to bottom of the file, and replace
 #'../naev-atwork_back/naev-artwork/3D/ships/' and
 # './ship_polygon_3d/' by the right paths in naev repo.
 # If the ship is big, go in the function "polygonify_all_ships"
@@ -14,13 +14,13 @@
 # Rem : in this case, it's not useful to add it to the dict.
 # If your ship is medium (destroyer, courrier...), use (10,40)
 # If your ship is big (cruiser, bulk...), use (12,50)
-# Special case of medium/big ships with a simple shape: 
+# Special case of medium/big ships with a simple shape:
 # you may use (10,30) or (12,30)
 
 # First parameter should be increased when the ship is long
 # Second parameter should be increased when the shape of the ship is complex
 
-# Then,run the script from shell 
+# Then,run the script from shell
 # (or any other interface that prints warnings).
 # If the script refines the polygon a lot (more than 10-15 times), there
 # is maybe something wrong. To see what parameters to use,
@@ -34,7 +34,7 @@
 # a/ Each point of the STL is rotated and projected to create the 2D shape
 #    for different angles.
 # b/ For each angular value, a set of points is created in all the rectangle
-#    containing the points of the projected STL. Each of these points is 
+#    containing the points of the projected STL. Each of these points is
 #    activated only if it is inside a triangle of the STL.
 
 # 3 ) The set of points is transformed into a polygon in polygonFromPng.
@@ -51,34 +51,34 @@ import os
 import subprocess
 
 # Create the projections of the ship from the STL data
-def pointsFromSTL( adress, sx, size, center, alpha ) :
+def pointsFromSTL( address, sx, size, center, alpha ) :
     # Extract the mesh and the points
 
-    shipMesh = mesh.Mesh.from_file(adress)
+    shipMesh = mesh.Mesh.from_file(address)
     v0 = np.transpose(shipMesh.v0) # TODO : take the center into account
     v1 = np.transpose(shipMesh.v1)
     v2 = np.transpose(shipMesh.v2)
-    
+
     # Compute x and y max and min
     xM = max( [np.amax(v0[0,:]), np.amax(v1[0,:]), np.amax(v2[0,:])] )
     xm = min( [np.amin(v0[0,:]), np.amin(v1[0,:]), np.amin(v2[0,:])] )
     yM = max( [np.amax(v0[1,:]), np.amax(v1[1,:]), np.amax(v2[1,:])] )
     ym = min( [np.amin(v0[1,:]), np.amin(v1[1,:]), np.amin(v2[1,:])] )
-    
+
     # Rescale the data
     leng   = max(xM-xm,yM-ym)
     factor = size/leng
-    
+
     v0 = factor*v0
     v1 = factor*v1
     v2 = factor*v2
-    
+
     # Rotate the stuff for any angle
     dtheta = 2*math.pi/sx**2
-    
+
     xlist = []
     ylist = []
-    
+
     for it in range(sx**2):
         # Rotate the points
         theta = it*dtheta + math.pi/2
@@ -88,7 +88,7 @@ def pointsFromSTL( adress, sx, size, center, alpha ) :
         vt0 = rot * v0
         vt1 = rot * v1
         vt2 = rot * v2
-        
+
         # Projection for the view
         proj = np.matrix([[1, 0, 0],\
                           [0, 1, math.tan(alpha)],\
@@ -96,7 +96,7 @@ def pointsFromSTL( adress, sx, size, center, alpha ) :
         vt0 = proj * vt0
         vt1 = proj * vt1
         vt2 = proj * vt2
-        
+
         x0 = vt0[0,:] # Extract x and y coordiantes
         x1 = vt1[0,:]
         x2 = vt2[0,:]
@@ -115,7 +115,7 @@ def pointsFromSTL( adress, sx, size, center, alpha ) :
         xgrid = []
         ygrid = []
         fullDots = np.zeros( ( int(xmax)-int(xmin)+1 , int(ymax)-int(ymin)+1 ) )
-        
+
         for ai, i in enumerate( range(int(xmin),int(xmax)+1) ):
             for aj, j in enumerate( range(int(ymin),int(ymax)+1) ):
                 # Test if there is a triangle for which the point (i,j) is inside
@@ -124,15 +124,15 @@ def pointsFromSTL( adress, sx, size, center, alpha ) :
                 D2 = np.multiply(x2-i,y0-j) - np.multiply(x0-i,y2-j);
                 D3 = np.multiply(x0-i,y1-j) - np.multiply(x1-i,y0-j);
                 D0 = D1+D2+D3;
-                
+
                 j1 = np.where(np.multiply(D0,D1) > 0)[1];
                 j2 = np.where(np.multiply(D0,D2) > 0)[1];
                 j3 = np.where(np.multiply(D0,D3) > 0)[1];
                 j4 = np.intersect1d( np.intersect1d(j1,j2) , j3);
-                
+
                 # Hell, there are flat triangles. As a consequence, > cannot
                 # be replaced by >= in np.where
-                
+
                 if len(j4) >= 1: # point is in a triangle
                     fullDots[ai,aj] = 1
 
@@ -153,14 +153,14 @@ def pointsFromSTL( adress, sx, size, center, alpha ) :
                     else:
                         xgrid.append(i)
                         ygrid.append(j)
-                    
+
         xlist.append(xgrid)
         ylist.append(ygrid)
-        
+
         #plt.scatter(xgrid,ygrid)
         #plt.scatter(x0.tolist()[0],y0.tolist()[0])
         #break
-        
+
         # A nice progress bar
         progress = str( int(100 * (it / sx**2)) )
         nbars = int(50 * it / sx**2)
@@ -170,56 +170,56 @@ def pointsFromSTL( adress, sx, size, center, alpha ) :
     bar = '=' * 50
     progress = 100
     print('\rTransforming STL... [%s] %s%%' % (bar, progress), end = '\n')
-                    
+
     return (xlist,ylist,factor)
 
 
 # Computes a polygon from STL
-def polygonFromSTL(adress,sx,scale,center,alpha,minlen,maxlen):
-    points  = pointsFromSTL(adress,sx,scale,center,alpha)
+def polygonFromSTL(address,sx,scale,center,alpha,minlen,maxlen):
+    points  = pointsFromSTL(address,sx,scale,center,alpha)
     xlist  = points[0]
     ylist  = points[1]
     factor = points[2]
     polygon = polygonFromPoints((xlist,ylist),minlen,maxlen)
-    
+
     # Rescale by dividing by factor
     xlist = polygon[1]
     ylist = polygon[2]
     xlist = [np.array(i)/factor for i in xlist]
     ylist = [np.array(i)/factor for i in ylist]
-    
+
     xpoint = points[0]
     ypoint = points[1]
     xpoint = [np.array(i)/factor for i in xpoint]
     ypoint = [np.array(i)/factor for i in ypoint]
-    
+
     return ( (xpoint,ypoint), (polygon[0],xlist,ylist) )
 
 # Computes a polygon from Blender
-def polygonFromBlend(adress,sx,scale,center,alpha,minlen,maxlen):
+def polygonFromBlend(address,sx,scale,center,alpha,minlen,maxlen):
     # Export from Blender to STL
     ret = subprocess.run([os.environ.get('BLENDER', 'blender'), address, '--background', '--python', 'blend_to_stl.py'])
     if ret.returncode != 0:
         print("Warning: STL export failed.\
               probably the required blender file does not exist")
-    
-    stlAdress = (os.path.basename(adress) + ".stl")
-    
+
+    stlAdress = (os.path.basename(address) + ".stl")
+
     # Generate the polygon
     out = polygonFromSTL(stlAdress,sx,scale,center,alpha,minlen,maxlen)
-    
+
     # Remove the intermediate stl file
     os.remove(stlAdress)
-    
+
     return out
-    
+
 
 # Generates polygon for all ships via stl
 def polygonify_all_ships_stl(blendPath, polyPath, overwrite):
-    
+
     # Default parameters
     default_params = (8,30)
-    
+
     # First define the parameters for special files
     params = {
                "archimedes.blend" : (12,50),
@@ -251,19 +251,19 @@ def polygonify_all_ships_stl(blendPath, polyPath, overwrite):
                "vigilance_dvaered.blend" : (10,40),
                "watson.blend" : (12,50),
               }
-    
+
     for fileName in os.listdir(blendPath):
         if (fileName.endswith(".blend")):
-            
+
             # Remove the .png
             name = os.path.splitext(fileName)[0]
-            
+
             polyAdress = (polyPath+name+".xml")
-            
+
             # Test if the file already exists
             if ( not overwrite and os.path.exists(polyAdress) ) :
                 continue
-                
+
             # Manage parameters
             sx   = default_params[0]
             np   = default_params[1]
@@ -273,27 +273,27 @@ def polygonify_all_ships_stl(blendPath, polyPath, overwrite):
                 np   = mNm[1]
 
             blendAdress  = (blendPath+fileName)
-     
+
             print("Generation of " + polyAdress + ". Parameters are : ("\
                    + str(sx) + ", " + str(np) + ")")
-            
+
             pntNplg = polygonFromBlend(blendAdress,sx,np,[0,0,0],math.pi/4,3,6)
-            
+
             polygon = pntNplg[1]
             generateXML(polygon,polyAdress)
 
 
 if __name__ == "__main__":
-    
-    polygonify_all_ships_stl("../naev-atwork_back/naev-artwork/3D/ships/", "./ship_polygon_3d/", 0)
-    
+
+    polygonify_all_ships_stl("../ships/", "./ship_polygon_3d/", 0)
+
 #    pntNplg = polygonFromBlend(\
 #        "../naev-atwork_back/naev-artwork/3D/ships/watson.blend",\
 #        12,50,[0,0,0],math.pi/4,3,6)
-#    
+#
 #    points  = pntNplg[0]
 #    polygon = pntNplg[1]
 #    poly    = polygon[0]
-#    
+#
 #    plt.scatter(points[0][0],points[1][0])
 #    plt.scatter(polygon[1][0],polygon[2][0])
